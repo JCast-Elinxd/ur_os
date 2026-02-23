ff398d4fe6f7434f6c5d957d668e336264469881
NosoyJuannnn Castrillon <137562803+JCast-Elinxd@users.noreply.github.com>
2026-02-23 23:59:18 +0000

    SJF Preemptive pre1

--- src/ur_os/SJF_P.java
+++ src/ur_os/SJF_P.java
@@ -19,23 +19,63 @@ public class SJF_P extends Scheduler{
         super(os);
 
     }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty){// When a NEW process enters the queue, process in CPU, if any, is extracted to compete with the rest
-        
-    } 
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty){// When a process return from IO and enters the queue, process in CPU, if any, is extracted to compete with the rest
-        
-    } 
+    public void newProcess(boolean cpuEmpty) {
+
+        if (!processes.isEmpty()) {
+
+            processes.sort((p1, p2) ->
+                Integer.compare(
+                    p1.getRemainingTimeInCurrentBurst(),
+                    p2.getRemainingTimeInCurrentBurst()
+                )
+            );
+
+            Process shortest = processes.get(0);
+
+            if (!cpuEmpty) {
+
+                Process current = os.getProcessInCPU();
+
+                if (shortest.getRemainingTimeInCurrentBurst() <
+                    current.getRemainingTimeInCurrentBurst()) {
+
+                    processes.remove(0); // remover antes
+
+                    os.interrupt(
+                        InterruptType.SCHEDULER_CPU_TO_RQ,
+                        shortest
+                    );
+
+                    return;
+                }
+            }
+        }
+
+        getNext(cpuEmpty);
+    }
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+        newProcess(cpuEmpty);
+    }
     
    
     @Override
     public void getNext(boolean cpuEmpty) {
-        
-        //Insert code here
 
-     }
+        if (!processes.isEmpty() && cpuEmpty) {
+
+            processes.sort((p1, p2) ->
+                Integer.compare(p1.getRemainingTimeInCurrentBurst(), p2.getRemainingTimeInCurrentBurst())
+            );
+
+            Process p = processes.get(0);
+            processes.remove(0);
+
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
+        }
+    }
  
 }
