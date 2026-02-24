e5b9675623a1f7e028d0df2712a91ff31c458ed9
Julieta Montoya Quintero <julieta.montoya@urosario.edu.co>
2026-02-24 04:52:23 +0000

    Refactor SJF_NP class: clean up code structure and improve readability

--- src/ur_os/SJF_NP.java
+++ src/ur_os/SJF_NP.java
@@ -2,31 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package ur_os;
 
-/**
- *
- * @author prestamour
- */
-public class SJF_NP extends Scheduler{
+public class SJF_NP extends Scheduler {
 
-    
-    SJF_NP(OS os){
+    public SJF_NP(OS os) {
         super(os);
     }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        
-       //Insert code here
-        
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive
-    
+    public void getNext(boolean cpuEmpty) {
+
+        if (!processes.isEmpty() && cpuEmpty) {
+            
+            Process shortest = processes.get(0);
+            
+            for (Process p : processes) {
+                int tiempoP = p.getPbl().getRemainingTimeInCurrentBurst();
+                int tiempoShortest = shortest.getPbl().getRemainingTimeInCurrentBurst();
+                
+                if (tiempoP < tiempoShortest) {
+                    shortest = p;
+                } 
+
+                else if (tiempoP == tiempoShortest) {
+                    shortest = tieBreaker(shortest, p);
+                }
+            }
+            
+            processes.remove(shortest);
+            
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, shortest);
+        }
+    }
+
+    @Override
+    public void newProcess(boolean cpuEmpty) {
+
+    }
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+    }
 }
